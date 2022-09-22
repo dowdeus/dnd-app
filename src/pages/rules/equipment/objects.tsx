987d53feffc4aab832651f958a777669e8e02748
RedHyena <adixon88@sky.com>
2022-09-22 15:17:31 +0100

    Objects page built

---
+++ src/pages/rules/equipment/objects.tsx
@@ -0,0 +1,38 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+import Markdown from 'markdown-to-jsx';
+import { useRulesBySection } from '../../../data-access/useRules';
+import styles from '../../../styles/Rules.module.scss';
+
+const useGetRules = (index:string) => {
+  const { data, isLoading, isError } = useRulesBySection(index);
+
+  if (isLoading) return '..loading';
+  if (isError) return '..error';
+  return data;
+};
+
+const Objects = () => {
+  const rule = useGetRules('objects');
+  const ruleDesc = (item:string) => {
+    if (item === undefined) return '';
+    return item;
+  };
+
+  return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div className={styles.ruleGrid}>
+                    <Link href='/rules'>
+                        <button className={styles.backButton}>
+                            <a>{'<Back'}</a>
+                        </button>
+                    </Link>
+                    <Markdown options={{ wrapper: Fragment }}>{ruleDesc(rule.desc)}</Markdown>
+                </div>
+            </div>
+        </div>
+  );
+};
+
+export default Objects;
