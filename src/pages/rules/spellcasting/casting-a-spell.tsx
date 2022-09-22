6734efdfee4bbdabc9b76945c4961e734a541a1f
RedHyena <adixon88@sky.com>
2022-09-22 17:58:00 +0100

    Created pages for Spellcasting

---
+++ src/pages/rules/spellcasting/casting-a-spell.tsx
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+import Markdown from "markdown-to-jsx";
+import { useRulesBySection } from "../../../data-access/useRules";
+import BackButton from "../../../components/BackButton/BackButton";
+import styles from '../../../styles/Rules.module.scss';
+
+const useGetRules = (index:string) => {
+    const {data, isLoading, isError} = useRulesBySection (index);
+    
+    if (isLoading) return '..loading';
+    if (isError) return '..error';
+    return data;
+}
+
+const CastingASpell = () => {
+    const rule = useGetRules ('casting-a-spell');
+    const ruleDesc = (item:string) => {
+        if (item === undefined) return '';
+        return item;
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div className={styles.ruleGrid}>
+                    <BackButton href='/rules'></BackButton>
+                    <Markdown options={{wrapper:Fragment}}>{ruleDesc(rule.desc)}</Markdown>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CastingASpell;
