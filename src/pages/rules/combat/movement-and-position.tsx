b21c0e1f620d80e5f5f4839a2ed50f65cabdf063
Kris <cfkristophskee@gmail.com>
2022-09-22 20:16:21 +0100

    feat: complete combat section

---
+++ src/pages/rules/combat/movement-and-position.tsx
@@ -0,0 +1,34 @@
+import { Fragment } from 'react';
+import Markdown from 'markdown-to-jsx';
+import { useRulesBySection } from '../../../data-access/useRules';
+import styles from '../../../styles/Rules.module.scss';
+import BackButton from '../../../components/BackButton/BackButton';
+
+const useGetRules = (index: string) => {
+  const { data, isLoading, isError } = useRulesBySection(index);
+
+  if (isLoading) return '..Loading';
+  if (isError) return '...Error';
+  return data;
+};
+
+const MovementAndPosition = () => {
+  const rule = useGetRules('movement-and-position');
+  const ruleDesc = (item: string) => {
+    if (item === undefined) return '';
+    return item;
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <div className={styles.ruleGrid}>
+            <BackButton href='/rules'></BackButton>
+            <Markdown options={{ wrapper: Fragment }}>{ruleDesc(rule.desc)}</Markdown>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovementAndPosition;
