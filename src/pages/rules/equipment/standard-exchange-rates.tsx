ad0c69598968a915cd2d1ce0de7c9497d5aab36d
RedHyena <adixon88@sky.com>
2022-09-22 14:53:47 +0100

    File linted

--- src/pages/rules/equipment/standard-exchange-rates.tsx
+++ src/pages/rules/equipment/standard-exchange-rates.tsx
@@ -1,36 +1,36 @@
-import { Fragment } from "react";
-import Link from "next/link";
-import Markdown from "markdown-to-jsx";
-import { useRulesBySection } from "../../../data-access/useRules";
+import { Fragment } from 'react';
+import Link from 'next/link';
+import Markdown from 'markdown-to-jsx';
+import { useRulesBySection } from '../../../data-access/useRules';
 import styles from '../../../styles/Rules.module.scss';
 
-const useGetRules=(index:string) => {
-    const {data, isLoading, isError}=useRulesBySection(index);
+const useGetRules = (index:string) => {
+  const { data, isLoading, isError } = useRulesBySection(index);
 
-    if (isLoading) return'..loading';
-    if (isError) return'..error';
-    return data;
-}
+  if (isLoading) return '..loading';
+  if (isError) return '..error';
+  return data;
+};
 
-const StandardExchangeRates=() => {
-    const rule=useGetRules('standard-exchange-rates');
-    const ruleDesc=(item:string) => {
-        if (item === undefined) return'';
-        return item;
-    }
+const StandardExchangeRates = () => {
+  const rule = useGetRules('standard-exchange-rates');
+  const ruleDesc = (item:string) => {
+    if (item === undefined) return '';
+    return item;
+  };
 
-    return (
+  return (
         <div className={styles.container}>
             <div className={styles.main}>
                 <div className={styles.rulesGrid}>
                     <Link href='/rules'><button className={styles.backButton}>
                         <a>{'<Back'}</a></button>
                     </Link>
-                    <Markdown options={{wrapper:Fragment}}>{ruleDesc(rule.desc)}</Markdown>
+                    <Markdown options={{ wrapper: Fragment }}>{ruleDesc(rule.desc)}</Markdown>
                 </div>
             </div>
         </div>
-    )
-}
+  );
+};
 
 export default StandardExchangeRates;
